fc9abd8b3528cffde0e24f5723bc30fc815cef22
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-08-16 15:58:42 +0300

    fix marks

--- specs/main.spec.js
+++ specs/main.spec.js
@@ -2,16 +2,13 @@
 const searchField = $('.search-box');
 const menu = $('div.nav-toggle-btn');
 const menuHotelReff = $('#Hotel');
+const startPage = 'https://www.bellagio.com';
 
 describe('bellagio.com ', () => {
-
-
-    const startPage = 'https://www.bellagio.com';
 
     beforeEach(() => {
         browser.get(startPage);
     });
-
 
     it('should have a text - "BOOK A ROOM"', () => {
         expect($('div.nav-booking').getText()).toContain('BOOK A ROOM');
@@ -21,7 +18,6 @@ describe('bellagio.com ', () => {
         menu.click();
         expect(menuHotelReff.getText()).toContain('HOTEL');
     });
-
 
     it('should open menu with search field at the top', () => {
         menu.click();
