44dc73323a1f13e191ca57fdb8cbc43b4eae6246
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-08-16 15:51:27 +0300

    three its have been done

--- specs/main.spec.js
+++ specs/main.spec.js
@@ -1,19 +1,30 @@
 'use strict';
+const searchField = $('.search-box');
+const menu = $('div.nav-toggle-btn');
+const menuHotelReff = $('#Hotel');
+
+describe('bellagio.com ', () => {
 
 
-describe('onliner', () => {
-
-    const startPage = 'https://www.onliner.by';
+    const startPage = 'https://www.bellagio.com';
 
     beforeEach(() => {
-        browser.get(startPage)
+        browser.get(startPage);
     });
 
 
-    it('should have title', () => {
-        browser.get(startPage);
-        //expect(page.pageTitle.getText()).toEqual('RESTAURANTS');
-        //expect(page.resultsWrapper.isPresent()).toBe(true);
-        console.log('I am here');
+    it('should have a text - "BOOK A ROOM"', () => {
+        expect($('div.nav-booking').getText()).toContain('BOOK A ROOM');
+    });
+
+    it('can choose room in field - "reservation"', () => {
+        menu.click();
+        expect(menuHotelReff.getText()).toContain('HOTEL');
+    });
+
+
+    it('should open menu with search field at the top', () => {
+        menu.click();
+        expect(searchField.isDisplayed()).toBeTruthy();
     });
 });
